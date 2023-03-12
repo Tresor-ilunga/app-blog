25beec82657243f8ac9ff7b4d0e433a23e55ec2c
Tresor-ilunga <19im065@esisalama.org>
2023-03-12 13:54:28 +0100

    Prohibit access to users who are not logged in from liking

---
+++ assets/scripts/like.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export default class Like {
+    constructor(likeElements) {
+        this.likeElements = likeElements;
+
+        if (this.likeElements) {
+            this.init();
+        }
+    }
+
+    init() {
+        this.likeElements.map(element => {
+            element.addEventListener('click', this.onClick)
+        })
+    }
+
+    onClick(event) {
+        event.preventDefault();
+        const url = this.href;
+
+        axios.get(url).then(res => {
+            console.log(res, this);
+            const nb = res.data.nbLike;
+            const span = this.querySelector('span');
+
+            this.dataset.nb = nb;
+            span.innerHTML = nb + ' J\'aime';
+
+            const thumbsUpFilled = this.querySelector('svg.filled');
+            const thumbsUpUnfilled = this.querySelector('svg.unfilled');
+
+            thumbsUpFilled.classList.toggle('hidden');
+            thumbsUpUnfilled.classList.toggle('hidden');
+        })
+    }
+}
